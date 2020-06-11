94cd29c4d875bf26313383080bf01bde9f8f6b56
[yuyh] <[1576246851@qq.com]>
2020-06-11 17:03:37 +0800

    【新增】myerror

--- raise/myerror.py
+++ raise/myerror.py
@@ -4,15 +4,10 @@ class MyError(Exception):
 class DBError(Exception):
     pass
 
-raise MyError('this is my error')
-raise DBError('this is db error')
 
-from apyori import apriori
-data = [['豆奶','莴苣'],
-        ['莴苣','尿布','葡萄酒','甜菜'],
-        ['豆奶','尿布','葡萄酒','橙汁'],
-        ['莴苣','豆奶','尿布','葡萄酒'],
-        ['莴苣','豆奶','尿布','橙汁']]
-result = list(apriori(transactions=data))
+if __name__ == '__mian__':
+    raise MyError('this is my error')
+    raise DBError('this is db error')
+
 
 
